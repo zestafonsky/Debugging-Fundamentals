da31d08935918f2258e15964e8b972602ab1ac80
George Kaladze <George_Kaladze@epam.com>
2022-03-05 19:55:57 +0400

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+        //Allows to override method with argument type as Object only  
+	  	public override bool Equals(object obj)  
+	    {
+            if (obj == null)
+                return false;
+            var emp = (Product)obj;  
+		  return emp.Name == Name && emp.Price == Price;  
+		}
+        public override int GetHashCode() => new { Name, Price }.GetHashCode();
+    }
+}
---
+++ Task1/Utilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Sorts an array in ascending order using bubble sort.
+        /// </summary>
+        /// <param name="numbers">Numbers to sort.</param>
+        public static void Sort(int[] numbers)
+        {
+            if (numbers is null)
+            {
+                throw new ArgumentNullException();
+            }
+            int temp;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[j];
+                        numbers[j] = numbers[i];
+                        numbers[i] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// based on a <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="products">Products used for searching.</param>
+        /// <param name="predicate">Product predicate.</param>
+        /// <returns>If match found then returns index of product in <paramref name="products"/>
+        /// otherwise -1.</returns>
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            if (products is null || predicate is null )
+            {
+                throw new ArgumentNullException();
+            }
+            try
+            {
+                for (int i = 0; i < products.Length; i++)
+                {
+                    var product = products[i];
+                    if (predicate(product))
+                    {
+                        return i;
+                    }
+                }
+            }
+            catch (Exception ) { return -1; }
+            return -1;
+        }
+    }
+}
